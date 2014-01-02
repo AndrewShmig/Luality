36e0ceaf58ea6ec718bfe0bf8422decffa3d463e
AndrewShmig <andrewshmig@gmail.com>
2014-01-02 17:22:51 +0400

    fixed table.filter

--- luality/table/filter.lua
+++ luality/table/filter.lua
@@ -7,7 +7,7 @@
 --               return value%2 == 0 and true or false	 
 --            end
 -- local even = table.filter(t, fn)
--- print(table.unpack(even)) -- {2, 4}
+-- print(table.unpack(even)) -- {nil, 2, nil, 4}
 
 -- returns a new table with values for which fn function returned true
 function table.filter(self, fn)
@@ -19,7 +19,7 @@ function table.filter(self, fn)
     end
   end
   
-  return table.remove_nils(new_table)
+  return new_table
 end
 
 -- return
